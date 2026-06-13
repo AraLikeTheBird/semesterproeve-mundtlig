2271103fc811384e3015ca3bb1dbdacad530456f
ara <arw001@edu.zealand.dk>
2026-06-13 20:35:40 +0200

    now it's time for one last bow, like all your other selves. Eleven's hour is over now. The clock is striking twelve's.

--- creatorjs.js
+++ creatorjs.js
@@ -388,6 +388,14 @@ function updateAvatarLayer(value) {
     // track state
     if (layerType === "body") currentBodyModel = value;
     if (layerType === "head") currentHeadModel = value;
+
+    const neutralValues = ["empty-default", "start-image"];
+
+    img.classList.remove("neutral-layer");
+
+    if (neutralValues.includes(value)) {
+        img.classList.add("neutral-layer");
+    }
 }
 
 function bodyOrHead(value) {
@@ -532,6 +540,36 @@ function setDefaultOption() {
     updateAvatarLayer(currentOption);
 }
 
+const ripple = document.getElementById("themeRipple");
+const btn = document.getElementById("themeToggle");
+
+let darkMode = false;
+
+btn.addEventListener("click", () => {
+
+    // 1. GET POSITION
+    const rect = btn.getBoundingClientRect();
+
+    ripple.style.left = rect.left + rect.width / 2 + "px";
+    ripple.style.top = rect.top + rect.height / 2 + "px";
+
+    // 2. START RIPPLE
+    ripple.classList.remove("active");
+    void ripple.offsetWidth;
+    ripple.classList.add("active");
+
+    // 3. SWITCH THEME MID-ANIMATION
+    setTimeout(() => {
+        darkMode = !darkMode;
+
+        document.body.classList.toggle("dark", darkMode);
+    }, 325);
+
+    // 4. CLEANUP
+    setTimeout(() => {
+        ripple.classList.remove("active");
+    }, 400);
+});
 /* =========================
    INIT
 ========================= */
